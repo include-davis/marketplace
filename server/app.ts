4d8a697efcb506f413119d293b5fd0c6c5f8e35d
aaronb12-ux <afairster03@gmail.com>
2026-02-02 13:44:38 -0800

    added express-mongo connection

--- server/app.ts
+++ server/app.ts
@@ -1,1 +1,23 @@
-console.log("hello world!");
+import express from "express"
+import cors from "cors"
+import type { Application } from "express"
+import { startMongoClient } from "./services/mongoService.ts"
+import { loadEnvFile } from "process"
+
+loadEnvFile(".env") //load env file
+
+const app: Application = express()
+app.use(cors())
+app.use(express.json())
+
+async function setupClient() { //connect to mongo client
+    const client = await startMongoClient();
+    app.locals.client = client;
+}
+
+setupClient()
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
