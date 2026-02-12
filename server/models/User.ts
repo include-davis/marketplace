a9b4cfa8b817bca72ca2e8c816b573d629fd3f2a
erikagosti <erika.gosti@gmail.com>
2026-02-11 22:42:28 -0800

    feat: created User.ts
    created schemas for user auth

---
+++ server/models/User.ts
@@ -0,0 +1,54 @@
+import mongoose, { Schema, Types, Model } from "mongoose";
+
+export interface GoogleProvider {
+    sub: string;
+    email?: String;
+}
+
+export interface UserDoc {
+    _id: Types.ObjectId;
+    email: String;
+    
+    passwordHash: string | null;
+    google: GoogleProvider | null;
+    
+    isEmailVerified: boolean;
+}
+
+const googleSchema = new Schema<GoogleProvider>(
+    {
+        sub: { type: String, index: true },
+        email: { type: String }
+    },
+    { _id: false }
+);
+
+const userSchema = new Schema<UserDoc>({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+        match: [/^\S+@\S+\.\S+$/, "Invalid email format"], // regex matching
+    },
+    
+    passwordHash: {
+        type: String,
+        default: null,
+        select: false,
+    },
+    
+    google: {
+        type: googleSchema,
+        default: null,
+    },
+    
+    isEmailVerified: { type: Boolean, default: false },
+});
+
+// create email indices starting from A in ascending order
+userSchema.index({ email: 1 }, { unique: true });
+
+export const User: Model<UserDoc> = mongoose.models.User || mongoose.model<UserDoc>("User", userSchema);
