4c6af9879feec168bd3360d0789499ba3c57a5df
aaronb12-ux <afairster03@gmail.com>
2026-02-23 03:08:46 -0800

    began working on live messaging feature. but only doing a simple version for testing

--- server/app.ts
+++ server/app.ts
@@ -6,6 +6,9 @@ import { loadEnvFile } from "process"
 import { createMessagesRouter } from "./routes/messagesRouter.ts"
 import listingsRouter from "./routes/listingsRouter.ts"
 import conversationsRouter from "./routes/conversationsRoutes.ts"
+import { createServer} from "http"
+import { Server } from "socket.io"
+
 
 
 loadEnvFile(".env") //load env file
@@ -13,6 +16,14 @@ loadEnvFile(".env") //load env file
 const app: Application = express()
 app.use(cors())
 app.use(express.json())
+
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
 
 
 async function setupClient() { //connect to mongo client
@@ -28,7 +39,10 @@ setupClient()
 app.use("/listings", listingsRouter);
 app.use("/conversations", conversationsRouter)
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
+
+
 })
