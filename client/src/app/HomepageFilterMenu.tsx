6690feab812c559fcc414b740b2909064224df21
Alex Yao <axyao@ucdavis.edu>
2026-04-15 21:39:54 -0700

    feat: Implemented Homepage Filter Menu component visually, no logic yet

---
+++ client/src/app/HomepageFilterMenu.tsx
@@ -0,0 +1,33 @@
+"use client";
+import styles from "./HomepageFilterMenu.module.scss";
+import React from "react";
+
+export default function HomepageFilterMenu() {
+    return (
+        <aside className={styles.filterMenu}>
+            <h1 className={styles.title}>Filters</h1>
+            
+            <div className={styles.dropdownGroup}>
+                <select className={styles.dropdown} defaultValue="">
+                    <option value="" disabled>
+                        Categories
+                    </option>
+                </select>
+
+                <select className={styles.dropdown} defaultValue="">
+                    <option value="" disabled>
+                        Price Range
+                    </option>
+                </select>
+
+                <select className={styles.dropdown} defaultValue="">
+                    <option value="" disabled>
+                        Sort By
+                    </option>
+                </select>
+            </div>
+
+            <button className={styles.applyButton}>Apply</button>
+        </aside>
+    );
+}
