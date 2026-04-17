56501fc603222775bd33e0d734cfdee7b27337c8
Alex Yao <axyao@ucdavis.edu>
2026-04-16 18:54:59 -0700

    Added dropdown options

--- client/src/app/HomepageFilterMenu.tsx
+++ client/src/app/HomepageFilterMenu.tsx
@@ -12,17 +12,56 @@ export default function HomepageFilterMenu() {
           <option value="" disabled>
             Categories
           </option>
+          <option value="Electrical Components">
+            Electrical Components
+          </option>
+          <option value="Tools">
+            Tools
+          </option>
+          <option value="Materials">
+            Materials
+          </option>
+          <option value="Fasteners">
+            Fasteners
+          </option>
         </select>
 
         <select className={styles.dropdown} defaultValue="">
           <option value="" disabled>
             Price Range
           </option>
+          <option value="Relevance">
+            Relevance
+          </option>
+          <option value="Newly Listed">
+            Newly Listed
+          </option>
+          <option value="Price: Low to High">
+            Price: Low to High
+          </option>
+          <option value="Price: High to Low">
+            Price: High to Low
+          </option>
         </select>
 
         <select className={styles.dropdown} defaultValue="">
           <option value="" disabled>
             Sort By
+          </option>
+          <option value="Under $250">
+            Under $250
+          </option>
+          <option value="$250 - $500">
+            $250 - $500
+          </option>
+          <option value="$500 - $750">
+            $500 - $750
+          </option>
+          <option value="$750 - $1000">
+            $750 - $1000
+          </option>
+          <option value="$1000+">
+            $1000+
           </option>
         </select>
       </div>
