9c44cdbffd6cbb5ccc68c26574ee18df945512f0
DrewCoding <drewwilliams9090@gmail.com>
2026-04-23 12:54:46 -0700

    Made the options button clickable

--- client/src/app/components/Option/Option.tsx
+++ client/src/app/components/Option/Option.tsx
@@ -6,13 +6,14 @@ import Image from "next/image";
 interface OptionProps {
     src: string;
     label: string;
+    onClick?: () => void;
 }
 
-export function Option({ src, label }: OptionProps) {
+export function Option({ src, label, onClick }: OptionProps) {
     return (
-        <div className={styles.buttonContainer}>
-            <Image src={src} alt={label} width={100} height={100} className={styles.menu} />
-            <div className={styles.buttonText}>{label}</div>
-        </div>
+        <button className={styles.buttonContainer} onClick={onClick}>
+            <Image src={src} alt={label} width={100} height={100} className={styles.buttonImage} />
+            <span className={styles.buttonText}>{label}</span>
+        </button>
     )
 }
