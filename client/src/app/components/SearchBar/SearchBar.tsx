a5b415761f501f1a333d91a5fe6b1c1b96809adb
DrewCoding <drewwilliams9090@gmail.com>
2026-04-26 19:49:56 -0700

    Converted raw svg html into a file within Search component

--- client/src/app/components/SearchBar/SearchBar.tsx
+++ client/src/app/components/SearchBar/SearchBar.tsx
@@ -1,29 +1,17 @@
 "use client";
 
+import Image from "next/image";
 import styles from "./SearchBar.module.scss";
 
 export function SearchBar() {
   return (
     <div className={styles.searchBarWrapper}>
-      <svg
-        className={styles.searchIcon}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <circle cx="11" cy="11" r="8" />
-        <line x1="21" y1="21" x2="16.65" y2="16.65" />
-      </svg>
+      <Image src="/Navbar/search-icon.svg" alt="search" width={80} height={80} className={styles.searchIcon} />
+
       <input
         type="text"
         className={styles.searchBar}
         placeholder="Search"
-      // value={searchQuery}
-      // onChange={(e) => setSearchQuery(e.target.value)}
       />
     </div>
   );
