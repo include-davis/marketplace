9e23e64ea832c79cd4c1e4b445bd1011cebfdcb4
Alex Yao <axyao@ucdavis.edu>
2026-05-07 12:51:29 -0700

    Small styling changes, added a color to globals

--- client/src/app/(pages)/(home)/page.tsx
+++ client/src/app/(pages)/(home)/page.tsx
@@ -1,10 +1,27 @@
-import Image from "next/image";
-import styles from "./page.module.scss";
-import React from "react";
+'use client';
+
+import Image from 'next/image';
+import styles from './page.module.scss';
+import React, { useState } from 'react';
+import HomePagination from '../../HomePagination';
 
 export default function Home() {
+  const totalPages = 10; // Temporary, will change once product listings are implemented
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
-    <>
-    </>
+    <div className={styles.pageWrapper}>
+      <h1 className={styles.searchHeading}>
+        Search Results for &ldquo;X&rdquo;
+      </h1>
+
+      <div className={styles.contentRow}>
+        <HomePagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
+      </div>
+    </div>
   );
 }
