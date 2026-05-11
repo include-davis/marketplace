dec951f10b56d48f7b0c9472a1779866c5ea008d
erikagosti <erika.gosti@gmail.com>
2026-05-11 16:16:09 -0700

    formatted page.module.scss and page.tsx with prettier

--- client/src/app/(pages)/create-post/page.tsx
+++ client/src/app/(pages)/create-post/page.tsx
@@ -1,45 +1,60 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import styles from "./page.module.scss";
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './page.module.scss';
 import FilterDropdown from '../(home)/_components/FilterDropdown/FilterDropdown';
 
-function TextField({ label, name, type = "text", placeholder }: { label: string; name: string; type?: string; placeholder?: string}) {
-    const [value, setValue] = useState("");
-    
-    return (
-      <div className={styles.field}>
-        <label htmlFor={name}>
-          {label} <span className={styles.requiredSymbol}>*</span>
-        </label>
-        <div className={styles.inputWrapper}>
-          <input
-            id={name}
-            name={name}
-            type={type}
-            value={value}
-            placeholder={placeholder}
-            onChange={(e) => setValue(e.target.value)}
-            className={styles.textFieldBox}
-          />
-        </div>
+function TextField({
+  label,
+  name,
+  type = 'text',
+  placeholder,
+}: {
+  label: string;
+  name: string;
+  type?: string;
+  placeholder?: string;
+}) {
+  const [value, setValue] = useState('');
+
+  return (
+    <div className={styles.field}>
+      <label htmlFor={name}>
+        {label} <span className={styles.requiredSymbol}>*</span>
+      </label>
+      <div className={styles.inputWrapper}>
+        <input
+          id={name}
+          name={name}
+          type={type}
+          value={value}
+          placeholder={placeholder}
+          onChange={(e) => setValue(e.target.value)}
+          className={styles.textFieldBox}
+        />
       </div>
-    );
-};
+    </div>
+  );
+}
 
 function DimensionField() {
   const [dimensions, setDimensions] = useState({
-    length: "", width: "", height: "",
+    length: '',
+    width: '',
+    height: '',
   });
-  
-  const handleChange = (field: "length" | "width" | "height", value: string) => {
+
+  const handleChange = (
+    field: 'length' | 'width' | 'height',
+    value: string,
+  ) => {
     setDimensions((prev) => ({
       ...prev,
       [field]: value,
     }));
-  }
+  };
   return (
     <div className={styles.field}>
       <label>Item Dimensions (in meters)</label>
@@ -76,85 +91,84 @@ function DimensionField() {
 }
 
 export default function CreatePostPage() {
-    return (
-      <main className={styles.page}>
-        <div className={styles.pageContainer}>
-          <Link href="/home" className={styles.back}>
-            <Image src={'/back_arrow.svg'} alt="<" width={32} height={32} />
-            <div className={styles.backLink}>Back</div>
-          </Link>
+  return (
+    <main className={styles.page}>
+      <div className={styles.pageContainer}>
+        <Link href="/home" className={styles.back}>
+          <Image src={'/back_arrow.svg'} alt="<" width={32} height={32} />
+          <div className={styles.backLink}>Back</div>
+        </Link>
 
-          <form className={styles.form}>
-            <div className={styles.title}>
-              <h2>List an item</h2>
+        <form className={styles.form}>
+          <div className={styles.title}>
+            <h2>List an item</h2>
+          </div>
+
+          <section className={styles.section}>
+            <h2 className={styles.sectionTitle}>Product Details</h2>
+
+            <div>
+              <TextField label="Title" name="Title" placeholder="Text" />
             </div>
 
-            <section className={styles.section}>
-              <h2 className={styles.sectionTitle}>Product Details</h2>
+            <div className={styles.textAreaField}>
+              <label htmlFor="Description">
+                Description <span className={styles.requiredSymbol}>*</span>
+              </label>
+              <textarea
+                rows={8}
+                placeholder="Give a brief description"
+                className={styles.textAreaBox}
+              ></textarea>
+            </div>
 
-              <div>
-                <TextField label="Title" name="Title" placeholder="Text" />
-              </div>
-
-              <div className={styles.textAreaField}>
-                <label htmlFor="Description">
-                  Description <span className={styles.requiredSymbol}>*</span>
+            <div className={styles.twoColumn}>
+              <div className={styles.dropdownField}>
+                <label>
+                  Category <span className={styles.requiredSymbol}>*</span>
                 </label>
-                <textarea
-                  rows={8}
-                  placeholder="Give a brief description"
-                  className={styles.textAreaBox}
-                ></textarea>
+                <div></div>
               </div>
-
-              <div className={styles.twoColumn}>
-                <div className={styles.dropdownField}>
-                  <label>
-                    Category <span className={styles.requiredSymbol}>*</span>
-                  </label>
-                  <div></div>
-                </div>
-                <div className={styles.dropdownField}>
-                  <label>
-                    Material Property{' '}
-                    <span className={styles.requiredSymbol}>*</span>
-                  </label>
-                  <div></div>
-                </div>
+              <div className={styles.dropdownField}>
+                <label>
+                  Material Property{' '}
+                  <span className={styles.requiredSymbol}>*</span>
+                </label>
+                <div></div>
               </div>
-
-              <div className={styles.halfWidth}>
-                <div className={styles.dropdownField}>
-                  <label>
-                    Condition <span className={styles.requiredSymbol}>*</span>
-                  </label>
-                  <div></div>
-                </div>
-              </div>
-
-              <DimensionField />
-
-              <div className={styles.priceWidth}>
-                <TextField
-                  label="Price"
-                  name="Price"
-                  type="number"
-                  placeholder="Add price"
-                />
-              </div>
-            </section>
-
-            <div className={styles.actions}>
-              <button type="button" className={styles.draftButton}>
-                Save as draft
-              </button>
-              <button type="submit" className={styles.previewButton}>
-                See Preview
-              </button>
             </div>
-          </form>
-        </div>
-      </main>
-    );
-}
 
+            <div className={styles.halfWidth}>
+              <div className={styles.dropdownField}>
+                <label>
+                  Condition <span className={styles.requiredSymbol}>*</span>
+                </label>
+                <div></div>
+              </div>
+            </div>
+
+            <DimensionField />
+
+            <div className={styles.priceWidth}>
+              <TextField
+                label="Price"
+                name="Price"
+                type="number"
+                placeholder="Add price"
+              />
+            </div>
+          </section>
+
+          <div className={styles.actions}>
+            <button type="button" className={styles.draftButton}>
+              Save as draft
+            </button>
+            <button type="submit" className={styles.previewButton}>
+              See Preview
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
