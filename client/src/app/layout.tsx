4d26d3b8b0ef306e7b79d1ea5b01852928086359
Sri Lakshmi Anbarasan <srilakshmi-anbarasan>
2026-05-10 18:47:31 -0700

    Fix root layout and global styles import

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./(pages)/_globals/globals.scss";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
