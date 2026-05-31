e1a0afe527098b924589ee221d26c9ef3faeda3b
Naomi Zhao <naomitan.zhao@gmail.com>
2026-05-31 13:44:18 -0700

    fix: Resolve my own comments

---
+++ client/src/app/(pages)/layout.tsx
@@ -0,0 +1,28 @@
+import './_globals/globals.scss';
+import React from 'react';
+import { Poppins } from 'next/font/google';
+import Navbar from "@/app/components/Navbar/Navbar";
+import Footer from '../components/Footer/Footer';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font1',
+  display: 'swap',
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={poppins.variable}>
+      <body>        
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
