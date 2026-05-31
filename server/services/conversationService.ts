b47ab37b1aedcebbad0d793512ae9e8cf36a8941
Naomi Zhao <naomitan.zhao@gmail.com>
2026-05-31 15:32:44 -0700

    feat: Rework conversation and messaging routes

--- server/services/conversationService.ts
+++ server/services/conversationService.ts
@@ -1,8 +1,8 @@
-import { Collection, Db, MongoClient } from 'mongodb';
+import { Collection, Db, MongoClient, ObjectId } from 'mongodb';
 
 export async function addConversation(
   client: MongoClient,
-  listingId: string,
+  listingId: ObjectId,
   user1id: string,
   user2id: string,
 ) {
@@ -11,8 +11,11 @@ export async function addConversation(
 
   const record = await collection.insertOne({
     users: [user1id, user2id],
-    listingId: listingId,
-    conversationid: user1id + '_' + user2id,
+    listingId,
+    user1id,
+    user2id,
+    createdAt: new Date(),
+    updatedAt: new Date(),
   });
 
   return record.insertedId;
@@ -24,5 +27,22 @@ export async function getConversationByListing(
 ) {
   const db: Db = client.db('MarketPlace');
   const collection: Collection = db.collection('Conversations');
-  const records = await collection.find({ listing: listingId }).toArray();
+  const record = await collection.findOne({ listing: listingId });
+  return record;
+}
+
+export async function updateConversationUpdated(
+  client: MongoClient,
+  listingId: string,
+) {
+  const db = client.db('MarketPlace');
+  const collection = db.collection('Conversations');
+  const filter = { _id: new ObjectId(listingId) };
+  const updateDoc = {
+    $set: {
+      updatedAt: new Date(),
+    },
+  };
+  const result = await collection.updateOne(filter, updateDoc);
+  return result;
 }
