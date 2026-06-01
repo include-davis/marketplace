aee4c7b43a34a07c3ac35a2a2f42222658189405
Naomi Zhao <naomitan.zhao@gmail.com>
2026-05-31 20:05:14 -0700

    refactor: Use abbreviated path for svg import for sell/active page

--- client/src/app/(pages)/(main)/sell/active/page.tsx
+++ client/src/app/(pages)/(main)/sell/active/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import styles from './page.module.scss';
-import RightArrow from '../../../../../../public/rightArrow.svg';
+import RightArrow from '@/../public/rightArrow.svg';
 import Listings from '../_components/Listings/Listings';
 
 export default function Active() {
