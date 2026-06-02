8e435bbebd3760c7013a622651d52cbf0c1cf7ef
Naomi Zhao <naomitan.zhao@gmail.com>
2026-06-02 01:01:58 -0700

    feat: Add mock data from Figma to view listing page

--- client/src/app/(pages)/(main)/listing/[id]/page.tsx
+++ client/src/app/(pages)/(main)/listing/[id]/page.tsx
@@ -1,15 +1,39 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './page.module.scss';
 import LeftArrow from '@/../public/leftArrow.svg';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const MOCK_IMG_SRCS = [
-  '/mock/listings/listings_mock1.jpg',
-  '/mock/listings/listings_mock2.jpg',
-  '/mock/listings/listings_mock3.jpg',
+  '/mock/listings/view_mock1.jpg',
+  '/mock/listings/view_mock2.jpg',
+  '/mock/listings/view_mock3.jpg',
 ];
 
+const MOCK_LISTING = {
+  title: 'Various machine parts',
+  desc: 'This bundle comes with various machine parts with several different applications. Includes rotators, chips, and satellite pieces applicable to machinery projects.',
+  price: 299,
+  category: 'Power Transmission',
+  materialProperty: 'Mechanical Property',
+  condition: 'Like new',
+  dimensions: '2 x 2 x 2 (m)',
+  createdAt: '2026-06-01T19:39:48.776Z',
+  updatedAt: '2026-06-01T19:39:48.776Z',
+  images: [
+    '/mock/listings/view_mock1.jpg',
+    '/mock/listings/view_mock2.jpg',
+    '/mock/listings/view_mock3.jpg',
+  ],
+  status: 'active',
+};
+
 export default function Listing() {
+  const [imageIdx, setImageIdx] = useState(0);
+  const listing = MOCK_LISTING;
+
   return (
     <div className={styles.page}>
       <Link href="/" className={styles.pageHeader}>
@@ -19,17 +43,20 @@ export default function Listing() {
       <div className={styles.pageContent}>
         <div className={styles.imageSection}>
           <div className={styles.imageWheel}>
-            {MOCK_IMG_SRCS.map((src) => (
-              <div className={styles.wheelImageWrapper}>
+            {MOCK_IMG_SRCS.map((src, idx) => (
+              <button
+                className={styles.wheelImageWrapper}
+                onClick={() => setImageIdx(idx)}
+              >
                 <Image src={src} alt={src} key={src} height={144} width={144} />
-              </div>
+              </button>
             ))}
           </div>
           <div className={styles.mainImageWrapperWrapper}>
             <div className={styles.mainImageWrapper}>
               <Image
-                src={MOCK_IMG_SRCS[1]}
-                alt={MOCK_IMG_SRCS[1]}
+                src={MOCK_IMG_SRCS[imageIdx]}
+                alt={MOCK_IMG_SRCS[imageIdx]}
                 fill
                 className={styles.mainImage}
               />
@@ -38,20 +65,15 @@ export default function Listing() {
         </div>
         <div className={styles.infoSection}>
           <div className={styles.infoBox}>
-            <h3>Product Name</h3>
+            <h3>{listing.title}</h3>
           </div>
           <div className={styles.infoBox}>
             <h3>Description</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur. Consectetur mi tempus
-              blandit quisque volutpat et. Ac sapien nulla sit vitae neque
-              mattis dictum nisi duis. Egestas amet vitae nam turpis turpis
-              auctor. Faucibus fusce vulputate faucibus porttitor nulla.
-            </p>
+            <p>{listing.desc}</p>
           </div>
           <div className={styles.infoPair}>
             <div className={styles.infoBox}>
-              <h3>Category</h3>
+              <h3>{listing.category}</h3>
             </div>
             <div className={styles.infoBox}>
               <h3>Type of Metal</h3>
@@ -59,18 +81,18 @@ export default function Listing() {
           </div>
           <div className={styles.infoPair}>
             <div className={styles.infoBox}>
-              <h3>Metal Property</h3>
+              <h3>{listing.materialProperty}</h3>
             </div>
             <div className={styles.infoBox}>
-              <h3>Condition</h3>
+              <h3>{listing.condition}</h3>
             </div>
           </div>
           <div className={styles.infoPair}>
             <div className={`${styles.infoBox} ${styles.infoBoxShort}`}>
-              <h3>Dimensions</h3>
+              <h3>{listing.dimensions}</h3>
             </div>
             <div className={`${styles.infoBox} ${styles.infoBoxShort}`}>
-              <h3>$X</h3>
+              <h3>${listing.price}</h3>
             </div>
           </div>
           <button className={styles.messageButton}>Message Seller</button>
