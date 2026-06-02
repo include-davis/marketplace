cdaa1618c567d10c4b40d8b2311ba738819378cb
Naomi Zhao <naomitan.zhao@gmail.com>
2026-06-02 15:53:04 -0700

    feat: Add page header components

--- client/src/app/(pages)/chat/[id]/page.tsx
+++ client/src/app/(pages)/chat/[id]/page.tsx
@@ -1,10 +1,20 @@
+import Link from 'next/link';
 import MessagesGrid from '../_components/messagesgrid/messagesgrid';
 import styles from './page.module.scss';
+import LeftArrow from '@/../public/leftArrow.svg';
+import Image from 'next/image';
+import Navbar from '@/app/_components/Navbar/Navbar';
 
 export default function Chat() {
   return (
     <div className={styles.page}>
-      <div className={styles.chatPageHeader}></div>
+      <Navbar />
+      <div className={styles.chatPageHeader}>
+        <Link href="/" className={styles.headerLink}>
+          <Image src={LeftArrow} alt="<" />
+          <span>Back to Listings</span>
+        </Link>
+      </div>
       <MessagesGrid />
     </div>
   );
