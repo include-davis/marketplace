145384b12c2b490b72d9ebd329da702178fe1c2a
Naomi Zhao <naomitan.zhao@gmail.com>
2026-06-02 15:01:26 -0700

    feat: Create FAQ page

--- client/src/app/(pages)/faq/_components/FaqDropdown/FaqDropdown.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './FaqDrop.module.scss';
-
-export default function FaqDropdown() {
-  return <div className={styles.faqDropdown}></div>;
-}
--- client/src/app/(pages)/faq/page.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./page.module.scss"
-
-export default function Faq() {
-    return <div className={styles.page}>
-        
-    </div>
-}
